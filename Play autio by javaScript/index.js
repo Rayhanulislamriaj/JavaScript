f00bacf5b62e6c5df097b6a4677ceaf4e2403979
Rayhanul Islam Riaj <riaj2517@gmail.com>
2023-10-08 23:00:33 +0600

    Updated files and added new features.

--- Play autio by javaScript/index.js
+++ Play autio by javaScript/index.js
@@ -1,27 +1,38 @@
-var len = document.querySelectorAll("button").length;
+// var len = document.querySelectorAll("button").length;
 
 
-for(var i=0; i < len; i++){
+for(var i=0; i < 3; i++){
     document.querySelectorAll(".my-button")[i].addEventListener("click", function(){
         var text = this.innerHTML;
         console.log(text);
         audioPlay(text);
+        playAnimation(text);
     });
+}
 
     function audioPlay(text){
         switch(text){
-            case "A":
+            case "a":
             var audio = new Audio("sounds/1.mp3");
             audio.play();
             break;
-             case "B":
+             case "b":
             var audio = new Audio("sounds/2.mp3");
             audio.play();
             break;
-             case "C":
+             case "c":
             var audio = new Audio("sounds/3.mp3");
             audio.play();
             break;
         }
-    };
+    }
+
+
+function playAnimation(text){
+    var selectedButton = document.querySelector("." +text);
+    selectedButton.classList.add("anim");
+
+    setTimeout(function (){
+        selectedButton.classList.remove("anim");
+    }, 1000);
 }
