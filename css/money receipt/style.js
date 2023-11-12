c43aa3e96699a41bb44163f4d4caa0701ef7183e
Rayhanul Islam Riaj <riaj2517@gmail.com>
2023-11-12 16:56:13 +0600

    Updated files and added new features.

---
+++ css/money receipt/style.js
@@ -0,0 +1,40 @@
+// let x = 7;
+// if(true){
+//     let x = 6;
+//     let x = 8;
+//     console.log(x);
+// }
+// console.log(x);
+
+// let carName;
+// carName = "Volvo";
+
+// var 5 = five;
+// var ten = 5 + 5 + "Five";
+// var 0range = 20;
+// console.log(ten);
+// let carName = console.log(ten);
+// let $
+
+
+    // let x = "Javascript";
+    // let x = "HTML";
+    // {
+    //     let var = "Hello world";
+    // }
+
+//     let age = 21;
+//      age = 20;
+// console.log(age);
+
+
+//    let x= 5 + 5;
+//    let y = "5" + 5;
+//    let z = "Hello" + 5;
+
+//    console.log(typeof x);
+//    console.log(typeof y);
+//    console.log(typeof z);
+//    const value = "Hello" && "World"
+let x = 29 % 9
+   console.log(x);
