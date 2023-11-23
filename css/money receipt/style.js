739af8508c427b6c6538aba409c9c6c50e5f8ddd
Rayhanul Islam Riaj <riaj2517@gmail.com>
2023-11-23 16:31:06 +0600

    Updated files and added new features.

--- css/money receipt/style.js
+++ css/money receipt/style.js
@@ -1,12 +1,57 @@
-function longestString(names){
-  let longestWord = '';
+// const person = {
+//   name: "John",
+//   age: 30,
+//   city: "New York"
+// };
 
-  for(name of names) {
-    if(name.length > longestWord.length){
-      longestWord = name;
-    }
+// const personArray = Object.values(person);
+
+// for (let i = 0; i < personArray.length; i++){
+//   console.log(personArray[i]);
+// }
+
+// console.log(JSON.stringify(person));
+
+// let c = "Hello Peter!";
+// console.log(c.toUpperCase());
+
+
+
+
+
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   language: "",
+//   get fullName(){
+//     return this.firstName + " " + this.lastName;
+//   }
+// };
+
+
+
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   fullName: function() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// };
+
+
+function Person(first, last, age) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.fullName = function(){
+    return this.firstName + ' ' + this.lastName;
   }
-  return [ longestWord, names.indexOf(longestWord)];
 }
 
-console.log(longestString(['foo', 'bar','riaj', 'MD Rayhanul Islam']));
+
+const riaj = new Person('Rayhanul Islam', 'Riaj', 25);
+const sumit = new Person('Sumit', 'Saha', 35);
+const Jadid = new Person('Jadid', 'Rahman', 25);
+
+riaj.country = 'Bangladesh';
+console.log(riaj);
