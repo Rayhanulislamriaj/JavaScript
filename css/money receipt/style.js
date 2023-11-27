e60b12ed8838eb014c7f7b0de0f69450c4449e93
Rayhanul Islam Riaj <riaj2517@gmail.com>
2023-11-27 16:56:50 +0600

    Updated files and added new features.

--- css/money receipt/style.js
+++ css/money receipt/style.js
@@ -1,60 +1,128 @@
-// array = [1, 3, 10, 11, 14];
-// goal = 13;
+//  const paymentSuccess = true;
+//  const marks = 80;
 
-// const twoSum = (array, goal) => {
-//   let mapOfNumbers = {};
-//   let twoIndexes = [];
+//  function enroll(callback){
+//   console.log('Course enrollment is in progress:');
 
-//   for(let i = 0; i < array.length; i++) {
-//     mapOfNumbers[array[i]] = i;
-//   }
-
-//   for(let i = 0; i < array.length; i++) {
-//     let target = goal - arr[i];
-//     if(mapOfNumbers[target] !== null && mapOfNumbers[target] !== i){
-//       twoIndexes.push(i);
-//       twoIndexes.push(mapOfNumbers[target]);
+//   setTimeout(function(){
+//     if(paymentSuccess){
+//       callback();
+//     } else {
+//       console.log('Payment Failed');
 //     }
-//   }
-//   // return twoIndexes;
-//   console.log(twoIndexes);
-// }
+//   }, 2000);
+//  }
+
+//  function progress(callback){
+//   console.log('Course on progress:');
+
+//   setTimeout(function(){
+//     if(marks >= 80) {
+//       callback();
+//     } else {
+//       console.log('You could not get enough marks to get the certificate!');
+//     }
+//   }, 3000);
+//  }
+
+//  function getCertificate(){
+//   console.log('Preparing certificate');
+
+//   setTimeout(function() {
+//     console.log('Congratulations! You got your certificate');
+//   }, 1000);
+//  }
+
+//  enroll(function(){
+//   progress(getCertificate);
+//  });
 
 
-// class Car {
-//   constructor(brand) {
-//     this.carname = brand;
-//   }
-//   present() {
-//     return 'I have a ' + this.carname;
-//   }
-// }
-
-// class Model extends Car {
-//   constructor(brand, mod) {
-//     super(brand);
-//     this.model = mod;
-//   }
-//   show() {
-//     return this.present() + ', it is a ' + this.model;
-//   }
-// }
-
-// let myCar = new Model("Ford", "Mustang");
-
-// console.log(myCar.show());
 
 
-class Car {
-  constructor(brand) {
-    this.carname = brand;
-  }
-  get cnam() {
-    return this.carname;
-  }
-  set cnam(x) {
-    this.carname = x;
-  }
+
+
+// // const status = true;
+
+// // console.log('Task 1');
+
+// // const promise = new Promise(function(resolve, reject) {
+// //   setTimeout(function(){
+// //     if(status) {
+// //       resolve('Task 2');
+// //     } else {
+// //       reject('failed!');
+// //     }
+// //   }, 2000);
+// // });
+
+// // promise
+// //       .then(function(value){
+// //         console.log(value);
+// //       })
+// //       .catch(function(err){
+// //         console.log(err);
+// //       });
+
+// // console.log('Task 3');
+
+
+
+const paymentSuccess = true;
+const marks = 80;
+
+function enroll(){
+ console.log('Course enrollment is in progress:');
+
+ const promise = new Promise(function(resolve, reject) {
+
+    setTimeout(function(){
+    if(paymentSuccess){
+      resolve();
+    } else {
+      reject('Payment Failed');
+    }
+  }, 2000);
+ });
+   return promise;
+
 }
 
-const myCar = new Car("Ford");
+
+function progress(){
+ console.log('Preparing certificate');
+   
+ const promise = new Promise(function (resolve, reject) {
+  setTimeout(function(){
+    if(marks >= 80) {
+      resolve();
+    } else {
+      reject('You could not get enough marks to get the certificate!');
+    }
+  }, 3000);
+ });
+
+ return promise;
+
+}
+
+ function getCertificate(){
+  console.log('Preparing certificate');
+
+  const promise = new Promise(function(resolve){
+    setTimeout(function() {
+      resolve('Congratulations! You got your certificate');
+    }, 1000);
+  });
+  return promise;
+ }
+
+ enroll()
+      .then(progress)
+      .then(getCertificate)
+      .then(function(value){
+        console.log(value);
+      })
+      .catch(function(){
+        console.log(err);
+      });
