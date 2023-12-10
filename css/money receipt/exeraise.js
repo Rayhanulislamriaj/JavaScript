b7dd5dc762b526c3d2732de80a7c26f5a1efe51e
Rayhanul Islam Riaj <riaj2517@gmail.com>
2023-12-10 16:23:46 +0600

    Updated files and added new features.

---
+++ css/money receipt/exeraise.js
@@ -0,0 +1,20 @@
+//JSON structure
+{"employees"[
+    {"firstName":"Rayhanul Islam", "lastName":"Riaj"},
+    {"firstName":"Nazim", "lastName":"Leeman"},
+    {"firstName":"Peter", "lastName":"Jones"}
+]}
+
+
+// XML structure
+<employees>
+    <employee>
+        <firstName>Rayhanul Islam</firstName> <lastName>Riaj</lastName>
+    </employee>
+    <employee>
+        <firstName>Nazim</firstName> <lastName>Leeman</lastName>
+    </employee>
+    <employee>
+        <firstName>Peter</firstName> <lastName>Jones</lastName>
+    </employee>
+</employees>
