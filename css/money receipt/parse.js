d26e1bf7b6fdaede5c28a3fa3cae290e5b6fc7ea
Rayhanul Islam Riaj <riaj2517@gmail.com>
2023-12-18 16:27:08 +0600

    Updated files and added new features.

--- css/money receipt/parse.js
+++ css/money receipt/parse.js
@@ -1,18 +1,34 @@
-const myobj = '{"name":"John", "age":30, "city":"New York"}';
+// const myobj = '{"name":"John", "age":30, "city":"New York"}';
 
-console.log(myobj);
-console.log(JSON.parse(myobj));
+// console.log(myobj);
+// console.log(JSON.parse(myobj));
 
-const text = '{"name":"John", "birth":"1986-12-14", "city":"New York"}';
-const obj = JSON.parse(text, function (key, value){
-    if(key === 'birth') {
-        return new Date(value);
-    } else {
-        return value;
-    }
-});
+// const text = '{"name":"John", "birth":"1986-12-14", "city":"New York"}';
+// const obj = JSON.parse(text, function (key, value){
+//     if(key === 'birth') {
+//         return new Date(value);
+//     } else {
+//         return value;
+//     }
+// });
 
-// obj.birth = new Date(obj.birth);
+// // obj.birth = new Date(obj.birth);
 
+
+// console.log(obj);
+
+
+const obj = {name: "John", age: 30, city: "New York"};
 
 console.log(obj);
+
+const myJSON = JSON.stringify(obj);
+
+console.log(myJSON);
+
+const arr = ["John", "Peter", "Sally", "Jane"];
+
+console.log(arr);
+
+const muArr = JSON.stringify(arr);
+console.log(muArr);
